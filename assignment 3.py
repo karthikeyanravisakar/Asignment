3f11af15b14e156ed365d54f2bc5a6141946d47a
karthikeyanravisakar <98077953+karthikeyanravisakar@users.noreply.github.com>
2022-01-20 11:50:57 +0530

    Add files via upload

---
+++ assignment 3.py
@@ -0,0 +1,7 @@
+name=input("enter your name: ")
+age=int(input("enter your age: "))
+designation=input("enter your designation: ")
+address=input("enter your address: ")
+lis=[name,age,designation,address]
+for i in lis:
+    print(i)
